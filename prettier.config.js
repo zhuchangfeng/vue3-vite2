517bdbbf6c33d577fc36c3f3c9cb843e99382b7f
zhuchangfeng <652165177@qq.com>
2021-06-24 18:07:29 +0800

    prettier.config

---
+++ prettier.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  printWidth: 100, // 每行代码长度（默认80）
+  tabWidth: 2, // 每个tab相当于多少个空格（默认2）
+  useTabs: false, // 是否使用tab进行缩进（默认false）
+  semi: true, // 声明结尾使用分号(默认true)
+  vueIndentScriptAndStyle: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  bracketSpacing: true, // 对象字面量的大括号间使用空格（默认true）
+  trailingComma: 'es5', // 多行使用拖尾逗号（默认none）
+  jsxBracketSameLine: false, // 多行JSX中的>放置在最后一行的结尾，而不是另起一行（默认false）
+  jsxSingleQuote: false,
+  arrowParens: 'always', // 只有一个参数的箭头函数的参数是否带圆括号（默认avoid
+  insertPragma: false,
+  requirePragma: false,
+  proseWrap: 'never',
+  htmlWhitespaceSensitivity: 'strict',
+  endOfLine: 'lf',
+  rangeStart: 0,
+};
