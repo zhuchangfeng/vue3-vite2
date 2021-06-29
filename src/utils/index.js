8149bd1e575be5f5ebcaf888292699cc0a110dd3
zhuchangfeng <652165177@qq.com>
2021-06-29 11:04:35 +0800

    工具文件

---
+++ src/utils/index.js
@@ -0,0 +1,30 @@
+const typer = Object.prototype.toString;
+/**
+ * @param {value} 待检查的值
+ * @description
+ * 获取值的类型
+ * @export
+ */
+export function getType(value) {
+  var typeStr = typer.call(value);
+  typeStr = typeStr.replace(/.*\s(\w+)\]/g, '$1');
+  return typeStr.toLowerCase();
+}
+/**
+ * 数组去重
+ * @param {目标数组:Array} arr
+ */
+export function uniq(arr) {
+  const result = [];
+  if (getType(arr) === 'array') {
+    const obj = {};
+    for (let i = 0; i < arr.length; i++) {
+      const elem = arr[i];
+      if (!obj[elem]) {
+        result.push(elem);
+        obj[elem] = 1;
+      }
+    }
+  }
+  return result;
+}
