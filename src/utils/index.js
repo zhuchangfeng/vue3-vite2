ad08940bfc77b0be80bb3c8d0a5fab6a040fb1b1
zhuchangfeng <652165177@qq.com>
2021-07-01 17:31:43 +0800

    添加深拷贝方法

--- src/utils/index.js
+++ src/utils/index.js
@@ -42,3 +42,57 @@ export const toNumber = (val) => {
   const n = parseFloat(val);
   return isNaN(n) ? val : n;
 };
+
+export const cloneLoop = (x) => {
+  const root = {};
+  // 栈
+  const loopList = [
+    {
+      parent: root,
+      key: undefined,
+      data: x,
+    },
+  ];
+
+  while (loopList.length) {
+    // 深度优先
+    const node = loopList.pop();
+    const parent = node.parent;
+    const key = node.key;
+    const data = node.data;
+
+    // 初始化赋值目标，key为undefined则拷贝到父元素，否则拷贝到子元素
+    let res = parent;
+    if (typeof key !== 'undefined') {
+      res = parent[key] = {};
+    }
+
+    for (let k in data) {
+      if (Object.prototype.hasOwnProperty.call(data, k)) {
+        if (typeof data[k] === 'object') {
+          // 下一次循环
+          loopList.push({
+            parent: res,
+            key: k,
+            data: data[k],
+          });
+        } else {
+          res[k] = data[k];
+        }
+      }
+    }
+  }
+
+  return root;
+};
+/**
+ * @description url参数拼接
+ */
+export const setObjToUrlParams = (baseUrl, obj) => {
+  let parameters = '';
+  for (const key in obj) {
+    parameters += key + '=' + encodeURIComponent(obj[key]) + '&';
+  }
+  parameters = parameters.replace(/&$/, '');
+  return /\?$/.test(baseUrl) ? baseUrl + parameters : baseUrl.replace(/\/?$/, '?') + parameters;
+};
