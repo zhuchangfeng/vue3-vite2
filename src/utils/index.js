d568f12173819c9e90844502f12aa1a3d87b6dca
zhuchangfeng <652165177@qq.com>
2021-07-02 17:57:04 +0800

    添加防抖节流

--- src/utils/index.js
+++ src/utils/index.js
@@ -96,3 +96,61 @@ export const setObjToUrlParams = (baseUrl, obj) => {
   parameters = parameters.replace(/&$/, '');
   return /\?$/.test(baseUrl) ? baseUrl + parameters : baseUrl.replace(/\/?$/, '?') + parameters;
 };
+/**
+ * @description 防抖
+ */
+export const debounce = (fuc, wait) => {
+  var timeout, result;
+  var debounce = function () {
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+    var self = this;
+    var args = arguments;
+    timeout = setTimeout(function () {
+      result = fuc.apply(self, args);
+    }, wait);
+    return result;
+  };
+  debounce.cancel = function () {
+    clearTimeout(timeout);
+    timeout = null;
+  };
+  return debounce;
+};
+
+export const throttle = (func, wait, options) => {
+  var timeout, context, args;
+  var previous = 0;
+  if (!options) options = {};
+  var later = function () {
+    previous = options.leading === false ? 0 : new Date().getTime();
+    timeout = null;
+    func.apply(context, args);
+    if (!timeout) context = args = null;
+  };
+  var throttled = function () {
+    var now = new Date().getTime();
+    if (!previous && options.leading === false) previous = now;
+    var remaining = wait - (now - previous);
+    context = this;
+    args = arguments;
+    if (remaining <= 0 || remaining > wait) {
+      if (timeout) {
+        clearTimeout(timeout);
+        timeout = null;
+      }
+      previous = now;
+      func.apply(context, args);
+      if (!timeout) context = args = null;
+    } else if (!timeout && options.trailing !== false) {
+      timeout = setTimeout(later, remaining);
+    }
+  };
+  throttled.cancel = function () {
+    clearTimeout(timeout);
+    previous = 0;
+    timeout = null;
+  };
+  return throttled;
+};
